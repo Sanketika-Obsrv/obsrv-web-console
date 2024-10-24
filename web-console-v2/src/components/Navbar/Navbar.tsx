4db44df256294e283bf406e8b963c8cb21cd9633
yashashk <yashashk@sanketika.in>
2024-10-24 14:58:49 +0530

    #OBS-I285 : Capitalize first letter apart from datasetId

--- web-console-v2/src/components/Navbar/Navbar.tsx
+++ web-console-v2/src/components/Navbar/Navbar.tsx
@@ -36,7 +36,7 @@ function BasicBreadcrumbs(): JSX.Element {
                     {pathnames.map((name, index) => {
                         const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
                         const isLast = index === pathnames.length - 1;
-                        const displayName = formatBreadcrumb(name);
+                        const displayName = isLast ? name : name.charAt(0).toUpperCase() + name.slice(1); // Capitalize first letter for non-last items
                         return isLast ? (
                             <Typography
                                 variant="body1"
