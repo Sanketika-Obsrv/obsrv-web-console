b2c0636d64b0a5cab3734c6fe25f5527c99690a6
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-10-24 17:18:46 +0530

    #OBS-I285: added max number of files selected for reupload

--- web-console-v2/src/pages/Dataset/wizard/ReUploadFiles.tsx
+++ web-console-v2/src/pages/Dataset/wizard/ReUploadFiles.tsx
@@ -190,8 +190,13 @@ const ReUploadFiles = (props: any) => {
         );
     };
 
+    const MAX_FILES = 10;
     const onSubmission = async () => {
         try {
+            if (!_.isEmpty(files) && _.size(files) > MAX_FILES) {
+                showAlert('Pre-signed URL generation failed: limit exceeded', 'error');
+                return;
+            }
             await onUpload(data);
         } catch (err) {
             setIsErrorUpload(true);
