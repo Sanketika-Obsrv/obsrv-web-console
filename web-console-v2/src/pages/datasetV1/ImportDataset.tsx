cad957504229e267915ad91c4a9f916fc545a0f8
Rakshitha-D <rakshithagowda.d@gmail.com>
2024-10-29 15:53:33 +0530

    #OBS-I296: dataset import data

--- web-console-v2/src/pages/datasetV1/ImportDataset.tsx
+++ web-console-v2/src/pages/datasetV1/ImportDataset.tsx
@@ -1,165 +1,60 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import { useAlert } from 'contexts/AlertContextProvider';
-import UploadFiles from 'pages/Dataset/wizard/UploadFiles';
-import { CardTitle, GenericCard } from 'components/Styled/Cards';
 import { Box, Button, Dialog, Grid, TextField, Typography } from '@mui/material';
-import RejectionFiles from 'components/Dropzone/RejectionFiles';
-import FilesPreview from 'components/Dropzone/FilesPreview';
+import { GenericCard } from 'components/Styled/Cards';
 import _ from 'lodash';
 import { readJsonFileContents } from 'services/utils';
 import localStyles from "../StepsPages/Ingestion/Ingestion.module.css";
 import AnimateButton from 'components/@extended/AnimateButton';
 import Loader from 'components/Loader';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PlaceholderContent from 'components/Dropzone/PlaceholderContent';
+import uploadIcon from 'assets/upload/upload_icon.svg';
+import { fetchDatasets } from 'services/datasetV1';
+import { DatasetStatus } from 'types/datasets';
 
 const ImportDataset = ({ open, onClose }: any) => {
-    const { data: dataState, files: filesState, config: configState } = {} as any;
-    const [data, setData] = useState(dataState);
-    const [datasetId, setDatasetId] = useState('');
-    const [files, setFiles] = useState(filesState);
-    const maxFileSizeConfig = 5242880;
-    const [fileErrors, setFileErrors] = useState<any>(null);
-    const [datasetName, setDatasetName] = useState('');
-    const [nameError, setNameError] = useState('');
-    const { showAlert } = useAlert();
-    const [loading, setLoading] = useState(false);
+    const flattenContents = (content: Record<string, any> | any) => {
+        const flattenedData = _.filter(_.flattenDeep(content), (field: any) => !_.isEmpty(field));
+        return flattenedData;
+    }
 
-    const flattenContents = (content: Record<string, any> | any) =>
-        _.filter(_.flattenDeep(content), (field: any) => !_.isEmpty(field));
-
-    const onFileUpload = async (uploadedFiles: File[]) => {
-        const contents = await Promise.all(
-            _.map(uploadedFiles, (file: File) => readJsonFileContents(file))
-        );
-
-        console.log('contents', contents)
+    const onDrop = useCallback(async (acceptedFiles: any[]) => {
+        const contents = await Promise.all(acceptedFiles.map((file: File) => readJsonFileContents(file)));
+        console.log(contents)
         const flattenedContents = flattenContents(contents);
-        console.log('flattendcontents', flattenedContents);
-
-        if (flattenedContents.length > 0 && _.get(flattenedContents, "name") || '') {
-            setDatasetName(_.get(flattenedContents, "name") || '');
-        }
-
-        setFiles(uploadedFiles);
-        setData(flattenedContents);
-    };
-
-    const onRemoveAll = () => {
-        setFiles(null);
-        setData(null);
-        setDatasetName('');
-    };
-
-    const nameRegex = /^[^!@#$%^&*()+{}[\]:;<>,?~\\|]*$/;
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue = e.target.value;
-        if (nameRegex.test(newValue)) {
-            setDatasetName(newValue);
-            setNameError('');
-        } else {
-            setNameError('The field should exclude any special characters, permitting only alphabets, numbers, ".", "-", and "_".');
-        }
-    };
-
-    const onFileRemove = async (file: File | string) => {
-        const filteredItems = files && files.filter((_file: any) => _file !== file);
-        const contents = await Promise.all(filteredItems.map((file: File) => readJsonFileContents(file)));
-        const flattenedContents = flattenContents(contents);
-        if (_.size(flattenedContents) === 0) {
-            setFiles(filteredItems);
-            setData(flattenedContents);
-            if (!_.isEmpty(filteredItems)) showAlert('Invalid file contents', "error");
-        } else {
-            setFiles(filteredItems);
-            setData(flattenedContents);
-        }
-    };
-
+        if (_.size(flattenedContents) === 0) throw new Error("Invalid file content");
+        console.log('flattendContents', flattenedContents);
+        const datasetData = await fetchDatasets({ data: { filters: {  } } });
+        console.log(datasetData);
+    }, [])
+    const { getRootProps, getInputProps } = useDropzone({ onDrop })
     return (
-        <Dialog open={open} onClose={onClose} fullWidth maxWidth="lg">
-        <Box>
-            {loading && <Loader loading={loading}/>}
-            <GenericCard className={localStyles.datasetDetails}>
-                <Box className={localStyles?.heading}>
-                    <Typography variant='h1'>Dataset Details</Typography>
-                </Box>
-
-                <Grid container spacing={3} className={localStyles?.gridContainer}>
-                    <Grid item xs={12} sm={6} lg={6}>
-                        <TextField
-                            name={'name'}
-                            label={'Dataset Name'}
-                            value={datasetName}
-                            onChange={handleNameChange}
-                            required
-                            variant="outlined"
-                            fullWidth
-                            error={Boolean(nameError)}
-                            helperText={nameError}
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={6} lg={6}>
-                        <TextField
-                            name={'dataset_id'}
-                            label={'Dataset ID'}
-                            value={datasetId}
-                            required
-                            variant="outlined"
-                            fullWidth
-                            disabled
-                        />
-                    </Grid>
-                </Grid>
-            </GenericCard>
-            <GenericCard>
-                <UploadFiles
-                    data={data}
-                    setData={setData}
-                    files={files}
-                    setFiles={setFiles}
-                    maxFileSize={maxFileSizeConfig}
-                    datasetImport={true}
-                    allowSchema
-                    subscribeErrors={setFileErrors}
-                    onUpload={onFileUpload}
+        <Dialog  fullWidth={true} open={open} onClose={onClose}>
+            <Box {...getRootProps()}>
+                <input {...getInputProps()} />
+                <PlaceholderContent
+                    imageUrl={uploadIcon}
+                    mainText="Upload Sample Data"
+                    subText="JSON"
+                    type="upload"
                 />
-                {!_.isEmpty(files) && (
-                    <Box mx={3} mt={18}>
-                        <Box display="flex" justifyContent="space-between">
-                            <Typography variant="h5" mt={1.5}>
-                                Files Uploaded
-                            </Typography>
-                            <Button variant="text" onClick={onRemoveAll}>
-                                <Typography variant="buttonText">
-                                    Remove All
-                                </Typography>
-                            </Button>
-                        </Box>
-                        <FilesPreview
-                            files={files}
-                            showList={false}
-                            onRemove={onFileRemove}
-                        />
-                    </Box>
-                )}
-                <Box sx={{ marginTop: 30, mr: 1, ml: 1, mb: 1 }}>
-                    {fileErrors?.length > 0 && <RejectionFiles fileRejections={fileErrors} />}
-                </Box>
-            </GenericCard>
+            </Box>
             <Box display="flex" justifyContent="flex-end">
                 <AnimateButton>
                     <Button
                         variant="contained"
                         sx={{ my: 2, ml: 1 }}
                         type="submit"
-                    disabled={loading || _.isEmpty(data)}
                     >
                         Proceed
                     </Button>
                 </AnimateButton>
             </Box>
-        </Box>
         </Dialog>
-    );
+    )
 };
 
 export default ImportDataset;
